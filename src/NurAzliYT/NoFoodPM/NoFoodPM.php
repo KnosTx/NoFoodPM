979631414e2e4f5c085854498dd3977e78f40a86
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-08-25 11:07:06 +0700

    Initial commit

--- src/NurAzliYT/NoFoodPM/NoFoodPM.php
+++ src/NurAzliYT/NoFoodPM/NoFoodPM.php
@@ -8,51 +8,64 @@ use pocketmine\event\player\PlayerExhaustEvent;
 use pocketmine\event\player\PlayerItemConsumeEvent;
 use pocketmine\item\ItemTypeIds;
 use pocketmine\plugin\PluginBase;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\player\Player;
 
 class NoFoodPM extends PluginBase implements Listener
 {
-	public function onEnable(): void
-	{
-		$this->saveDefaultConfig();
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
+    public function onEnable(): void
+    {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 
-	/**
-	 * @param PlayerItemConsumeEvent $event
-	 * @ignoreCancelled true
-	 */
-	public function onConsume(PlayerItemConsumeEvent $event)
-	{
-		$player = $event->getPlayer();
-		$lvlFolName = $player->getWorld()->getFolderName();
-		$itemId = $event->getItem()->getId();
-		$lvlACName = (array)$this->getConfig()->get("worlds");
-		$aItemAC = (array)$this->getConfig()->get("allowedFood");
-		if ($player->hasPermission("nofood.bypass")) return;
-		if (!in_array($lvlFolName, $lvlACName)) return;
-		foreach ($aItemAC as $aItem) {
-			$uAItem = strtoupper($aItem);
-			$aItemId = 0;
-			try {
-				$aItemId = constant(ItemIds::class . "::$uAItem");
-			} catch (Exception $e) {
-				if (is_int($aItem)) $aItemId = $aItem;
-			}
-			if ($itemId === $aItemId) return;
-		}
-		$event->setCancelled();
-	}
+    /**
+     * @param PlayerItemConsumeEvent $event
+     * @ignoreCancelled true
+     */
+    public function onConsume(PlayerItemConsumeEvent $event): void
+    {
+        $player = $event->getPlayer();
+        if (!$player instanceof Player) return;
+        $lvlFolName = $player->getWorld()->getFolderName();
+        $itemType = $event->getItem()->getTypeId();
+        $lvlACName = (array)$this->getConfig()->get("worlds");
+        $aItemAC = (array)$this->getConfig()->get("allowedFood");
 
-	/**
-	 * @param PlayerExhaustEvent $event
-	 * @ignoreCancelled true
-	 */
-	public function onExhaust(PlayerExhaustEvent $event)
-	{
-		if (!((bool)$this->getConfig()->get("noHungry"))) return;
-		$lvlFolName = $event->getPlayer()->getWorld()->getFolderName();
-		$lvlACName = (array)$this->getConfig()->get("worlds");
-		if (!in_array($lvlFolName, $lvlACName)) return;
-		$event->setCancelled();
-	}
+        if ($player->hasPermission("nofood.bypass")) return;
+        if (!in_array($lvlFolName, $lvlACName, true)) return;
+
+        foreach ($aItemAC as $aItem) {
+            $uAItem = strtoupper($aItem);
+            $aItemType = ItemTypeIds::AIR; // Default to AIR in case of failure
+            try {
+                $aItemType = constant(ItemTypeIds::class . "::$uAItem");
+            } catch (Exception $e) {
+                if (is_int($aItem)) $aItemType = $aItem;
+            }
+
+            if ($itemType === $aItemType) return;
+        }
+
+        $event->cancel();
+    }
+
+    /**
+     * @param PlayerExhaustEvent $event
+     * @ignoreCancelled true
+     */
+    public function onExhaust(PlayerExhaustEvent $event): void
+    {
+        if (!(bool)$this->getConfig()->get("noHungry")) return;
+
+        $player = $event->getPlayer();
+        if (!$player instanceof Player) return;
+
+        $lvlFolName = $player->getWorld()->getFolderName();
+        $lvlACName = (array)$this->getConfig()->get("worlds");
+        
+        if (!in_array($lvlFolName, $lvlACName, true)) return;
+        
+        $event->cancel();
+    }
 }
